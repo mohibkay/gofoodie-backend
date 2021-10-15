cccb29cf5953c245adcadc0a4189a58f0bc6961b
Mohib Khan <mohibk.in@gmail.com>
2021-10-15 15:43:35 +0530

    feat: add user model

---
+++ models/user.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    first_name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    last_name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      validate(value) {
+        if (!value.match(/^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/)) {
+          throw new Error("Email is not valid.");
+        }
+      },
+    },
+    street: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    state: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    city: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    pincode: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
